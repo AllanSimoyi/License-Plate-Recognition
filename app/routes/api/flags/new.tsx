c56a4db4314575cf41dbb85f25e6d5c90f5559ff
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-11-29 16:30:26 +0200

    Made changes

--- app/routes/api/flags/new.tsx
+++ app/routes/api/flags/new.tsx
@@ -3,6 +3,7 @@ import { json } from "@remix-run/node";
 import { z } from "zod";
 import { prisma } from "~/db.server";
 import { badRequest, DateSchema, PositiveIntSchema } from "~/lib/core.validations";
+import { FALLBACK_ERROR_MESSAGE } from "~/lib/errors";
 
 const Schema = z.object({
   vehicleId: PositiveIntSchema,
@@ -10,25 +11,32 @@ const Schema = z.object({
 });
 
 export async function action ({ request, params }: ActionArgs) {
-  const formData = await request.formData();
-  const fields = Object.fromEntries(formData.entries());
+  try {
+    const formData = await request.formData();
+    const fields = Object.fromEntries(formData.entries());
 
-  const result = await Schema.safeParseAsync(fields);
-  if (!result.success) {
-    const { formErrors, fieldErrors } = result.error.flatten();
-    return badRequest({ fields, fieldErrors, formError: formErrors.join(", ") });
-  }
-  const { vehicleId, dateTime } = result.data;
-
-  const flag = await prisma.flag.create({
-    data: {
-      vehicleId,
-      dateTime,
-    },
-    include: {
-      vehicle: true,
+    const result = await Schema.safeParseAsync(fields);
+    if (!result.success) {
+      const { formErrors, fieldErrors } = result.error.flatten();
+      console.log("formErrors", formErrors);
+      console.log("fieldErrors", fieldErrors);
+      return badRequest({ fields, fieldErrors, formError: formErrors.join(", ") });
     }
-  });
+    const { vehicleId, dateTime } = result.data;
 
-  return json({ flag });
+    const flag = await prisma.flag.create({
+      data: {
+        vehicleId,
+        dateTime,
+      },
+      include: {
+        vehicle: true,
+      }
+    });
+
+    return json({ flag });
+  } catch ({ message }) {
+    console.log("Caught Error", message as string);
+    return json({ errorMessage: message as string || FALLBACK_ERROR_MESSAGE }, { status: 200 });
+  }
 }
